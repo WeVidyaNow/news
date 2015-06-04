88664f7ca521a3bebbe51f8cb12be33edb0aec6e
Battleroid <admin@wevidyanow.com>
2015-06-04 18:31:53 -0400

    stuff got deleted

--- quote.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Only needed because the quote application doesn't put out content served with SSL
-$json = file_get_contents("http://wevidyanow.com:7000/quote");
-header("Content-Type: application/json");
-echo $json;
-?>
